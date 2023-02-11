71912734f884bc66e189b37eaeb7c842ec9b648b
Dashrath-Sharma <dashrathsharma7838@gmail.com>
2023-02-12 01:47:08 +0530

    CRUD completed without DB routes set

--- index.js
+++ index.js
@@ -7,75 +7,61 @@ const port = process.env.PORT || 3000
 const list = [
     {
         id:1,
-        name:"Dashrath",
-        age:"23",
-        email:"dashrath@gmail.com"
+        name:"todo1",
     },
     {
         id:2,
-        name:"Vishal",
-        age:"24",
-        email:"Vishal@gmail.com"
+        name:"todo2",
     },
     {
         id:3,
-        name:"Sachin",
-        age:"44",
-        email:"Sachin@gmail.com"
+        name:"todo3",
     },
     {
         id:4,
-        name:"Rajender",
-        age:"24",
-        email:"Rajender@gmail.com"
+        name:"todo4",
     },
     {
         id:5,
-        name:"pradeep",
-        age:"24",
-        email:"pradeep@gmail.com"
+        name:"todo5",
     },
 ]
 
 //get all users
-app.get('/api/v1/users', (req, res) => {
+app.get('/api/v1/tasks', (req, res) => {
     res.send(list)
 })
 
 //get single user
-app.get('/api/v1/users/:id', (req, res) => {
+app.get('/api/v1/tasks/:id', (req, res) => {
     const task = list.find(item => item.id == req.params.id)
     res.send(task)
 })
 
 //Add a new User
-app.post('/api/v1/users/add', (req, res) => {
+app.post('/api/v1/tasks/add', (req, res) => {
     const newCourse = {
         id:list.length+1,
-        name:req.body.name,
-        age:Math.floor(Math.random() * 50) + 10,
-        email:`${req.body.name}@gmail.com`
+        name:req.body.name
     }
     list.push(newCourse)
     res.send(list)
 })
 
 //Update a user
-app.put('/api/v1/users/update', (req, res) => {
+app.put('/api/v1/tasks/update', (req, res) => {
     if(!req.body.id) return res.status(400).send("Missing ID!")
     const index = list.find(n => n.id === parseInt(req.body.id))
     if(!index) return res.status(400).send("ID doesn't exist")
     index.name = req.body.name ? req.body.name : index.name
-    index.age = req.body.age ? req.body.age : index.age
-    index.email = req.body.email ? req.body.email : index.email
     res.send(index)
 })
 
 //Delete a user
-app.delete('/api/v1/users/delete/:id',(req, res) => {
+app.delete('/api/v1/tasks/delete/:id',(req, res) => {
     if(!req.params.id) return res.status(400).send("Missing ID")
     const index = list.find(n => n.id === parseInt(req.params.id))
-    res.send(last)
+    res.send(index)
 })
 
 app.listen(port, console.log(`server running on PORT ${port}`))
